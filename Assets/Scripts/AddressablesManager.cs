874cfa26ae7e0268c613773da3aa65b7a2f2169a
Spyros <spyros@traptics.com>
2024-01-18 20:44:57 +0200

    initial commit

---
+++ Assets/Scripts/AddressablesManager.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressablesManager : MonoBehaviour
+{
+    [SerializeField] AssetReference cubeReference;
+    [SerializeField] TMP_Text serverResponse;
+
+    GameObject cube;
+
+    private void Start()
+    {
+        // Initialize Addressables, at this point the the remote load path would be based on the StartupManager.BaseDomain that we set in the previous scene
+        Addressables.InitializeAsync().Completed += OnInitializeCompleted;
+    }
+
+    private void OnInitializeCompleted(AsyncOperationHandle<IResourceLocator> obj)
+    {
+        // Instantiate the cube prefab from the remote server
+        Addressables.InstantiateAsync(cubeReference).Completed += (handle) => {
+            cube = handle.Result;
+            cube.transform.position = new Vector3(0f, 0.5f, 0f);
+        };
+
+        GetServerResponse();
+    }
+
+    private void GetServerResponse()
+    {
+        // Web Get Request to StartupManager.BaseApiUrl/cubecolor
+        var request = new UnityEngine.Networking.UnityWebRequest($"{StartupManager.BaseApiUrl}/cubecolor.php");
+        request.downloadHandler = new UnityEngine.Networking.DownloadHandlerBuffer();
+        request.SendWebRequest().completed += (operation) =>
+        {
+            // Set the cube color based on the server response
+            string response = request.downloadHandler.text;
+            serverResponse.text = response;
+        };
+    }
+}
---
+++ Assets/Scripts/StartupManager.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartupManager : MonoBehaviour
+{
+    public static string BaseDomain { get; private set; } = @"http://www.spahar.me/";
+    public static string BaseApiUrl => $"{BaseDomain}/api";
+
+    private const string configFileName = "config.txt";
+
+    private void Awake()
+    {
+        // Get BaseDomain from an external config file
+        GetSettingsFromExternalConfig();
+
+        Debug.Log($"BaseDomain: {BaseDomain}");
+
+        // Load the main scene, after having set the BaseDomain
+        SceneManager.LoadSceneAsync("Main", LoadSceneMode.Single);
+    }
+
+    private void GetSettingsFromExternalConfig()
+    {
+        // Search for the file in the same folder as the executable (in the build) / in the project folder (in the editor)
+        string configPath = Path.Combine(Application.dataPath, "..", configFileName);
+
+        if (!File.Exists(configPath))
+        {
+            Debug.LogError("Config file not found!");
+            return;
+        }
+
+        string configContent = File.ReadAllText(configPath);
+        BaseDomain = configContent;
+    }
+}
